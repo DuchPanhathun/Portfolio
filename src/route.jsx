2afac6630c0966ec3c9275e05f642e293304692b
DuchPanhathun <duch.panhathun.2821@rupp.edu.kh>
2024-06-21 12:40:21 +0700

    ...

--- src/route.jsx
+++ src/route.jsx
@@ -3,11 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./page/home.jsx";
 import TestPage from "./page/test_page.jsx";
 import Menu from "./components/menu.jsx";
-import { HelmetProvider } from 'react-helmet-async';
 
 const Page = () => {
     return (
-        <HelmetProvider>
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
@@ -15,7 +13,6 @@ const Page = () => {
                     <Route path="/menu" element={<Menu />} />
                 </Routes>
             </Router>
-        </HelmetProvider>
     );
 };
 export default Page;
